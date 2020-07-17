c5daf7f9e157e2e04320a6d51e2fa39858feb2f4
Alan Jones <pastorajsm@gmail.com>
2020-07-17 11:40:47 -0700

    initial commit

--- Traveler.js
+++ Traveler.js
@@ -25,3 +25,41 @@ class Traveler {
 
     }
 }
+class Doctor extends Traveler {
+    constructor(name) {
+        super(name)
+    }
+    heal(traveler) {
+        return traveler.isHealthy = true
+
+    }
+}
+
+class Hunter extends Traveler {
+    constructor(name) {
+        super(name)
+        this.food = 2
+
+    }
+    hunt() {
+        this.food += 5
+    }
+    eat() {
+        if (this.food > 1) {
+            this.food -= 2
+        }
+        else {
+            this.isHealthy = false
+            this.food = 0
+
+        }
+    }
+
+    giveFood(traveler, numOfFoodUnits) {
+        if (numOfFoodUnits <= this.food) {
+            this.food -= numOfFoodUnits
+            traveler.food += numOfFoodUnits
+        }
+    }
+
+}
